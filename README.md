46cda7b3a621234422a5a284ad8ac408036f31ce
Drahman AbdulBasir <durmon.albasir@gmail.com>
2017-06-11 16:29:52 -0400

    add installation instructions for nodejs

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # or9-bleserver
 BLE server
+
+## Getting Started
+This application requires NodeJS. Ensure it's installed.  
+### Install NodeJS  
+```
+sudo apt-get install build-essentials
+wget https://nodejs.org/dist/v8.1.0/node-v8.1.0.tar.gz
+tar -xvf node-v8.1.0.tar.gz
+cd node-v8.1.0
+./configure
+make
+sudo make install
+```
+### Usage
--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 #!/usr/bin/env node
 "use strict";
 
+const bleno = require("bleno");
+const STATE = {
+	STATE_CHANGE: "stateChange",
+	AD_START: "advertisingStart",
+	POWERED_ON: "poweredOn"
+};
+const PRIMARY_SERVICE_UUID = "fffffffffffffffffffffffffffffff0";
+
+bleno.on(STATE.AD_START, adStart);
+bleno.on(STATE.STATE_CHANGE, stateChanged);
+
+function adStart (error) {
+	const descriptorConfig = {
+		uuid: "2901",
+		value: "someval"
+	};
+	const descriptor = new bleno.Descriptor(descriptorConfig);
+	const characteristicConfig = {
+		uuid: "fff1",
+		properties: [ "read", "write", "writeWithoutResponse" ],
+		value: "ff", // optional {Buffer}
+		descriptors: [ descriptor ]
+	};
+	const characteristic = new bleno.Characteristic(charConfig);
+	const primaryServiceConfig = {
+		uuid: PRIMARY_SERVICE_UUID,
+		characteristics: [ characteristic ]
+	};
+	const services = new bleno.PrimaryService(primaryService);
+	bleno.setServices(services);
+}
+
+function stateChanged (state) {
+	console.log(`BLE state changed ${state}`);
+	if (state === STATE.POWERED_ON) {
+		bleno.startAdvertising("myThing", PRIMARY_SERVICE_UUID);
+	} else {
+		bleno.stopAdvertising();
+	}
+}
