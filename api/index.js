01b4926c2663c853b5b307f35844152173d68a3d
Abdur Rahman <durmon.albasir@gmail.com>
2017-10-20 18:02:20 -0400

    pipe and stuff

--- api/index.js
+++ api/index.js
@@ -4,13 +4,18 @@
 
 const { spawnSync } = require("child_process");
 const { SWITCH, SENSOR } = require("../const");
+const STDIO = Object.freeze([
+	"pipe",
+	1,
+	2
+]);
 
 module.exports.PIN_STATE = Object.freeze({
 	get SWITCH () {
-		return spawnSync("gpio", ["read", SWITCH], { stdio: "pipe" }).stdout.toString();
+		return spawnSync("gpio", ["read", SWITCH], { stdio: STDIO }).stdout.toString();
 	},
 	get SENSOR () {
-		return spawnSync("gpio", ["read", SENSOR], { stdio: "pipe" }).stdout.toString();
+		return spawnSync("gpio", ["read", SENSOR], { stdio: STDIO }).stdout.toString();
 	},
 	get isDoorOpen () {
 		return !parseInt(this.SENSOR);
